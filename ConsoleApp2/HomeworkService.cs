b7a9581673e52f0611c08c4f0a553fedbb099d9c
DELL <theinhtetaung.developer@gmail.com>
2025-04-28 17:50:01 +0630

    feat: update

---
+++ ConsoleApp2/HomeworkService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace ConsoleApp2
+{
+    internal class HomeworkService
+    {
+       private readonly SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "DotNetTrainingBatch1",
+            UserID = "sa",
+            Password = "sa@123",
+            TrustServerCertificate = true
+        };
+        public void ReadDetail(int no)
+        {      
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            string qurey = $"select * from Tbl_Homewrok where No = {no}";
+            SqlCommand cmd = new SqlCommand(qurey, connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            connection.Close();
+        }
+        public void Read()
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            string qurey = "select * from Tbl_Homewrok";
+            SqlCommand cmd = new SqlCommand(qurey, connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            connection.Close();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using ConsoleApp2;
+using Microsoft.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+
+//SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+//sqlConnectionStringBuilder.DataSource = ".";
+//sqlConnectionStringBuilder.InitialCatalog = "DotNetTrainingBatch1";
+//sqlConnectionStringBuilder.UserID = "sa";
+//sqlConnectionStringBuilder.Password = "sa@123";
+//sqlConnectionStringBuilder.TrustServerCertificate = true;
+HomeworkService homewrokService = new HomeworkService();
+homewrokService.ReadDetail(1);
