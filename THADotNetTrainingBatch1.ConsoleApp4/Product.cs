8795db3c14ed841e24a55271d025296546092213
DELL <theinhtetaung.developer@gmail.com>
2025-05-07 20:02:54 +0630

    feat: ef.core

---
+++ THADotNetTrainingBatch1.ConsoleApp4/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace THADotNetTrainingBatch1.ConsoleApp4
+{
+    [Table("Tbl_Category")]
+    public class ProductCategory
+    {
+        [Key]
+        [Column("Id")]
+        public int ProductCategoryId { get; set; }
+        [Column("CategoryCode")]
+        public string ProductCategoryCode { get; set; }
+        [Column("Category")]
+        public string CategoryName { get; set; }
+    }
+
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server = , ;
+Database = DotNetTrainingBatch1;
+User Id = sa ;
+Password = sa@123 ;
+TrustServerCertificate = ture ");
+            }
+        }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
--- THADotNetTrainingBatch1.ConsoleApp4/Program.cs
+++ THADotNetTrainingBatch1.ConsoleApp4/Program.cs
@@ -1,23 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.Data.SqlClient;
+using THADotNetTrainingBatch1.ConsoleApp4;
 using THADotNetTrainingBatch1.Shared;
 
 Console.WriteLine("Hello, World!");
 
-AdoDotNetService service = new AdoDotNetService(
-    new SqlConnectionStringBuilder
-    {
-        DataSource = ".",
-        InitialCatalog = "DotNetTrainingBatch1",
-        UserID = "sa",
-        Password = "sa@123",
-        TrustServerCertificate = true,
-    });
-var lst = service.QueryV2<Product>("SELECT * FROM Tbl_Product");
-foreach (var item in lst )
+AppDbContext db = new AppDbContext();
+var lst = db.ProductCategories.ToList();
+
+foreach (var item in lst)
 {
-    Console.WriteLine(item.ProductName);
+    Console.WriteLine(item.CategoryName);
 }
+
+db.ProductCategories.Add(new ProductCategory
+{
+    ProductCategoryId = 2,
+    ProductCategoryCode = "P002",
+    CategoryName = "Vegetable"
+});
+//AdoDotNetService service = new AdoDotNetService(
+//    new SqlConnectionStringBuilder
+//    {
+//        DataSource = ".",
+//        InitialCatalog = "DotNetTrainingBatch1",
+//        UserID = "sa",
+//        Password = "sa@123",
+//        TrustServerCertificate = true,
+//    });
+//var lst = service.QueryV2<Product>("SELECT * FROM Tbl_Product");
+//foreach (var item in lst )
+//{
+//    Console.WriteLine(item.ProductName);
+//}
 
 public class Product
 {
