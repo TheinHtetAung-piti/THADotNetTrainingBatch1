9dba4a635157dab7bb2ea7c3933b06a936f884cd
DELL <theinhtetaung.developer@gmail.com>
2025-05-07 10:30:02 +0630

    feat: dapper

---
+++ THADotNetTrainingBatch1.ConsoleApp3/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using THADotNetTrainingBatch1.ConsoleApp3;
+
+Console.WriteLine("Hello, World!");
+SqlService sqlService = new SqlService();
+sqlService.Query<Product>("SELECT * FROM Tbl_Product");
+sqlService.Execute("SELECT * FROM Tbl_Product");
+sqlService.Execute("SELECT * FROM Tbl_Product");
+sqlService.Execute("SELECT * FROM Tbl_Product");
+//string query = @"INSERT INTO [dbo].[Tbl_Product]
+//           ([ProductCode]
+//           ,[ProductName]
+//           ,[Price]
+//           ,[Quantity]
+//           ,[CreateDateTime]
+//           ,[CreatedBy]
+//           )
+//     VALUES
+//           (@ProductCode
+//           ,@ProductName
+//           ,@Price
+//           ,@Quantity
+//           ,@CreateDateTime
+//           ,@CreatedBy
+//           )";
+//connection.Execute(query, new Product
+//{
+//    ProductCode = "P011",
+//    ProductName = "Pineapple",
+//    Price = 2000,
+//    Quantity = 30,
+//    CreateDateTime = DateTime.Now,
+//    CreatedBy = 1 
+//});
+//var lst = connection.Query<Product>("SELECT * FROM Tbl_Product").ToList();
+//foreach (var item in lst)
+//{
+//    Console.WriteLine(item.ProductId);
+//}
+
+public class Product
+{
+    public int ProductId { get; set; }
+    public string ProductCode { get; set; }
+    public string ProductName { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+
+    public DateTime CreateDateTime { get; set; }
+
+    public int CreatedBy { get; set; }
+}
---
+++ THADotNetTrainingBatch1.ConsoleApp3/SqlService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace THADotNetTrainingBatch1.ConsoleApp3
+{
+    internal class SqlService
+    {
+        private readonly SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "DotNetTrainingBatch1",
+            UserID = "sa",
+            Password = "sa@123",
+            TrustServerCertificate = true,
+        };
+        public List<T> Query<T>(string query, object? parameters = null)
+        {
+            using IDbConnection conneciton = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            conneciton.Open();
+            var lst = conneciton.Query<T>(query, parameters).ToList();
+            return lst;
+        }
+
+        public int Execute(string query, object? parameters = null)
+        {
+            using IDbConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);    
+            connection.Open();
+            int result = connection.Execute(query, parameters);
+            return result;
+        }
+    }
+
+}
