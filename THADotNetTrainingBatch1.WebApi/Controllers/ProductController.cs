620ddb68d17f5f6e30c4f53facb2142841fcfaac
DELL <theinhtetaung.developer@gmail.com>
2025-05-10 22:10:02 +0630

    feat: API

---
+++ THADotNetTrainingBatch1.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace THADotNetTrainingBatch1.WebApi.Controllers
+{
+    //https://Localhost:3000/api/Product
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProduct()
+        {
+            return Ok("GetProduct"); 
+        }
+
+        [HttpPost]
+        public IActionResult PostProduct()
+        {
+            return Ok("PostProduct");
+        }
+
+        [HttpPut]
+        public IActionResult PutProduct()
+        {
+            return Ok("PutProduct");
+        }
+
+        [HttpPatch]
+        public IActionResult PatchProduct()
+        {
+            return Ok("PatchProduct");    
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteProduct()
+        {
+            return Ok("Delete Product");
+        }
+    }
+}
