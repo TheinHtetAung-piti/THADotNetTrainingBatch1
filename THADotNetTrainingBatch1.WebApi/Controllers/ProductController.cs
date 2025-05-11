f00e4398bea79bbbf6b12b6d33aa00bac8b39b67
DELL <theinhtetaung.developer@gmail.com>
2025-05-11 23:58:23 +0630

    feat: API

--- THADotNetTrainingBatch1.WebApi/Controllers/ProductController.cs
+++ THADotNetTrainingBatch1.WebApi/Controllers/ProductController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using THADotNetTrainingBatch1.Shared;
+using THADotNetTrainingBatch1.WebApi.Model;
 
 namespace THADotNetTrainingBatch1.WebApi.Controllers
 {
@@ -8,34 +11,167 @@ namespace THADotNetTrainingBatch1.WebApi.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private readonly DapperService _dapperService;
+
+        public ProductController()
+            {
+            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+            { 
+                DataSource = ".",
+                InitialCatalog = "DotNetTrainingBatch1",
+                UserID = "sa",
+                Password = "sa@123",
+                TrustServerCertificate = true
+            };
+            _dapperService = new DapperService(sqlConnectionStringBuilder);
+            }
         [HttpGet]
         public IActionResult GetProduct()
         {
-            return Ok("GetProduct"); 
+        var lst = _dapperService.Query<ProductModel>("select * from Tbl_Product");
+            var data = new
+            {
+                IsSuccess = true,
+                Message = "Success",
+                Data = lst
+            };
+            return Ok(data); 
+        }
+
+        [HttpGet("Edit/{id}")]
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            string query = "select * from Tbl_Product where ProductId = @ProductId";
+            var lst = _dapperService.Query<ProductModel>(query,
+                new { 
+                        ProductId = id
+                    });
+
+            if (lst.Count == 0 )
+            {
+                return NotFound(new
+                {
+                    IsSuccess = false,
+                    Message = "Product Not Found!"
+                });
+            }
+            var data = new
+            {
+                IsSuccess = true,
+                Message = "Success",
+                Data = lst
+            };
+            return Ok(data);
         }
 
         [HttpPost]
-        public IActionResult PostProduct()
+        public IActionResult PostProduct([FromBody] ProductModel newProduct)
         {
-            return Ok("PostProduct");
+            newProduct.CreateDateTime = DateTime.Now;
+            newProduct.CreatedBy = 1;
+            string query = @"INSERT INTO [dbo].[Tbl_Product]
+           ([ProductCode]
+           ,[ProductName]
+           ,[ProductCategory]
+           ,[Price]
+           ,[Quantity]
+           ,[CreateDateTime]
+           ,[CreatedBy])
+     VALUES
+           (@ProductCode
+           ,@ProductName
+           ,@ProductCategory
+           ,@Price
+           ,@Quantity
+           ,@CreateDateTime
+           ,@CreatedBy)";
+            int result = _dapperService.Execute(query, newProduct);
+            var data = new
+            {
+                IsSuccess = result > 0 ,
+                Message = result > 0 ? "Success" : "Fail",
+            };
+            return Ok(data);
         }
 
         [HttpPut]
-        public IActionResult PutProduct()
+        [HttpPut("{id}")]
+        public IActionResult PutProduct([FromBody] ProductModel product,int id)
         {
-            return Ok("PutProduct");
+            string query = "select * from Tbl_Product where ProductId = @ProductId";
+            var lst = _dapperService.Query<ProductModel>(query,
+                new
+                {
+                    ProductId = id
+                });
+            if (lst.Count == 0)
+            {
+                return NotFound(new
+                {
+                    IsSuccess = false,
+                    Message = "Product Not Found!"
+                });
+            }
+
+            product.ModifiedBy = 1;
+            product.ModifiedDateTime = DateTime.Now;
+            string updQuery = @"UPDATE [dbo].[Tbl_Product]
+   SET [ProductCode] = @ProductCode
+      ,[ProductName] = @ProductName
+      ,[ProductCategory] = @ProductCategory
+      ,[Price] = @Price
+      ,[Quantity] = @Quantity
+      ,[ModifiedDateTime] = @ModifiedDateTime
+      ,[ModifiedBy] = @ModifiedBy
+ WHERE ProductId = @ProductId";
+            int result = _dapperService.Execute(updQuery, new { ProductId = id });
+            var data = new
+            {
+                IsSuccess = result > 0,
+                Message = result > 0 ? "Success" : "Fail",
+            };
+            return Ok(data);
         }
 
         [HttpPatch]
         public IActionResult PatchProduct()
         {
+
             return Ok("PatchProduct");    
         }
 
         [HttpDelete]
-        public IActionResult DeleteProduct()
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
         {
-            return Ok("Delete Product");
+            string query = "select * from Tbl_Product where ProductId = @ProductId";
+            var lst = _dapperService.Query<ProductModel>(query,
+                new
+                {
+                    ProductId = id
+                });
+            if (lst.Count == 0)
+            {
+                return NotFound(new
+                {
+                    IsSuccess = false,
+                    Message = "Product Not Found!"
+                });
+            }
+            string delQuery = @"delete from Tbl_Product where ProductId = @ProductId";
+            int result = _dapperService.Execute(delQuery, new
+            {
+                ProductId = id
+            });
+            var data = new
+            {
+                IsSuccess = result > 0,
+                Message = result > 0 ? "Success" : "Fail",
+            };
+            return Ok(data);
+
+
         }
     }
 }
---
+++ THADotNetTrainingBatch1.WebApi/Model/ProductModel.cs
@@ -0,0 +1,26 @@
+﻿namespace THADotNetTrainingBatch1.WebApi.Model
+{
+    public class ProductModel
+    {
+        public int ProductId { get; set; }  
+
+        public string ProductCode { get; set; }
+
+        public string ProductName { get; set; }  
+
+        public string ProductCategory { get; set; } 
+
+        public double Price { get; set; }   
+
+        public int Quantity { get; set; }   
+
+        public DateTime CreateDateTime { get; set; }    
+
+        public int CreatedBy { get; set; }  
+
+        public DateTime ModifiedDateTime { get; set; }  
+
+        public int ModifiedBy { get; set; } 
+
+    }
+}
