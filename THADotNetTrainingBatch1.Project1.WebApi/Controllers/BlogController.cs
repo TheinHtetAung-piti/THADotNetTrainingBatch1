c02aab51d5b4f4bf9d309b29e563648abe599995
DELL <theinhtetaung.developer@gmail.com>
2025-05-17 09:26:37 +0630

    feat: API

---
+++ THADotNetTrainingBatch1.Project1.WebApi/Controllers/BlogController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using THADotNetTrainingBatch1.Project1.Database.Models;
+
+namespace THADotNetTrainingBatch1.Project1.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly AppDbContext appDbContext;
+
+        public BlogController(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+    }
+}
