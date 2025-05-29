063a30cd5eb963878f142096cbd6f1677fec7334
DELL <theinhtetaung.developer@gmail.com>
2025-05-30 00:00:02 +0630

    feat: MVC

---
+++ THADotNetTrainingBatch1.MvcApp/Controllers/WalletController.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace THADotNetTrainingBatch1.MvcApp.Controllers
+{
+    public class WalletController : Controller
+    {
+        [ActionName("Index")]
+        public async Task<IActionResult> WalletIndex()
+        {
+            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder
+            {
+                DataSource = ".",
+                InitialCatalog = "MiniWallet",
+                UserID = "sa",
+                Password = "sa@123",
+                TrustServerCertificate = true
+            };
+            using IDbConnection db = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+            db.Open();
+            var lst = await db.QueryAsync<WalletModel>("select * from Tbl_Wallet");
+            return View("WalletIndex" , lst.ToList());
+        }
+
+        public class WalletModel()
+        {
+            public string WalletUserName {  get; set; } 
+
+            public string MobileNo { get; set; }    
+        }
+    }
+}
