efd4a804eb10130c459e9e89a3f3f3682d23afac
DELL <theinhtetaung.developer@gmail.com>
2025-05-30 12:53:33 +0630

    feat: CreateWallet

--- THADotNetTrainingBatch1.MvcApp/Controllers/WalletController.cs
+++ THADotNetTrainingBatch1.MvcApp/Controllers/WalletController.cs
@@ -7,26 +7,59 @@ namespace THADotNetTrainingBatch1.MvcApp.Controllers
 {
     public class WalletController : Controller
     {
+        private readonly SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder
+        {
+            DataSource = ".",
+            InitialCatalog = "MiniWallet",
+            UserID = "sa",
+            Password = "sa@123",
+            TrustServerCertificate = true
+        };
         [ActionName("Index")]
         public async Task<IActionResult> WalletIndex()
         {
-            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder
-            {
-                DataSource = ".",
-                InitialCatalog = "MiniWallet",
-                UserID = "sa",
-                Password = "sa@123",
-                TrustServerCertificate = true
-            };
+            
             using IDbConnection db = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
             db.Open();
             var lst = await db.QueryAsync<WalletModel>("select * from Tbl_Wallet");
             return View("WalletIndex" , lst.ToList());
         }
 
+        [ActionName("Create")]
+        public IActionResult WalletCreate()
+        {
+            return View("WalletCreate");
+        }
+
+        [HttpPost]
+        [ActionName("Create")]
+        public async Task<IActionResult> WalletCreateAsync(WalletModel requestModel )
+        {
+            string query = @"INSERT INTO [dbo].[Tbl_Wallet]
+           ([WalletUserName]
+           ,[FullName]
+           ,[MobileNo]
+           ,[Balance])
+     VALUES
+           (@WalletUserName
+           ,@FullName
+           ,@MobileNo
+           ,@Balance)";
+            using IDbConnection db = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+            db.Open();
+            await db.ExecuteAsync(query, requestModel);
+            return RedirectToAction("WalletCreate");
+        }
+
+
         public class WalletModel()
         {
+
             public string WalletUserName {  get; set; } 
+
+            public string FullName { get; set; }    
+
+            public decimal Balance { get; set; }
 
             public string MobileNo { get; set; }    
         }
