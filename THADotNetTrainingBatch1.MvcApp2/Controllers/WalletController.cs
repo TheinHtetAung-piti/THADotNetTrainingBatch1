686817153932014688a815698cb244fe0af84951
DELL <theinhtetaung.developer@gmail.com>
2025-06-09 01:24:49 +0630

    feat:Sectionasyn

---
+++ THADotNetTrainingBatch1.MvcApp2/Controllers/WalletController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace THADotNetTrainingBatch1.MvcApp2.Controllers
+{
+    public class WalletController : Controller
+    {
+        private readonly SqlConnectionStringBuilder sqlConnectionStringBuilder;
+        public WalletController(IConfiguration configuration)
+        {
+            sqlConnectionStringBuilder = new SqlConnectionStringBuilder(configuration.GetConnectionString("DbConnection"));
+        }
+        [ActionName("Index")]
+        public IActionResult WalletIndex()
+        {
+            return View();
+        }
+    }
+}
