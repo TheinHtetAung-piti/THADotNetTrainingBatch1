5fbd0577fc846b19cc8d68fa95432468037a5dc0
DELL <theinhtetaung.developer@gmail.com>
2025-06-09 12:49:54 +0630

    feat: index

---
+++ THADotNetTrainingBatch1.MvcApp3/Controllers/WalletController.cs
@@ -0,0 +1,57 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace THADotNetTrainingBatch1.MvcApp3.Controllers
+{
+    public class WalletController : Controller
+    {
+        private readonly SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder
+        {
+            DataSource = ".",
+            InitialCatalog = "MiniWallet",
+            UserID = "sa",
+            Password = "sa@123",
+            TrustServerCertificate = true
+        };
+
+        [ActionName("Index")]   
+        public IActionResult WalletIndex()
+        {
+            return View("WalletIndex");
+        }
+
+        [HttpPost]
+        [ActionName("Index")]
+        public async Task<IActionResult> WallerList()
+        {
+            try
+            {
+            using IDbConnection db = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+            db.Open();
+            var lst = await db.QueryAsync<WalletModel>("select * from Tbl_Wallet");
+            return Json(new { IsSuccess = true , Message = "Success" , Data = lst });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { IsSuccess = false, Message = ex.ToString() });
+            }
+        }
+    }
+
+    public class WalletModel()
+    {
+        public int WalletId { get; set; }
+
+        public string WalletUserName { get; set; }
+
+        public string FullName { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public string MobileNo { get; set; }
+    }
+}
--- THADotNetTrainingBatch1.MvcApp3/Program.cs
+++ THADotNetTrainingBatch1.MvcApp3/Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews().AddJsonOptions(opt =>
     opt.JsonSerializerOptions.PropertyNamingPolicy = null
-); 
+); ;
 
 var app = builder.Build();
 
